840b02dbf588bc4c51d2722afc8ac9df8975931a
Bob3_Chen <Bob3_Chen@asus.com>
2014-11-10 10:11:02 +0800

    add exit(X) Macro. fix some build code error.

--- client.c
+++ client.c
@@ -7,57 +7,86 @@
 #include <netinet/tcp.h>
 #include <netdb.h>
 
-#define SERV_TCP_PORT 8000 /* server's port */
+#define SERV_TCP_PORT 8100 //<asus-bob+>8000	/* server's port number */
+#define MAX_SIZE 80
+#define	exit(X)	return(X)	//<asus-bob+>
 
 int main(int argc, char *argv[])
 {
-  int sockfd;
-  struct sockaddr_in serv_addr;
-  char *serv_host = "localhost";
-  struct hostent *host_ptr;
-  int port;
-  int buff_size = 0;
+	int		sockfd;
+	struct sockaddr_in serv_addr;
+	char	*serv_host = "localhost";
+	struct hostent *host_ptr;
+	int		port;
+	int		buff_size = 0;
+//<asus-bob+>
+	char	msg[200];
+	int		getdata;
+	int		len;
+//<asus-bob+>
 
   /* command line: client [host [port]]*/
-  if(argc >= 2) 
-     serv_host = argv[1]; /* read the host if provided */
-  if(argc == 3)
-     sscanf(argv[2], "%d", &port); /* read the port if provided */
-  else 
-     port = SERV_TCP_PORT;
+	if(argc >= 2) 
+		serv_host = argv[1]; /* read the host if provided */
+	if(argc == 3)
+		sscanf(argv[2], "%d", &port); /* read the port if provided */
+	else 
+		port = SERV_TCP_PORT;
 
-  /* get the address of the host */
-  if((host_ptr = gethostbyname(serv_host)) == NULL) {
-     perror("gethostbyname error");
-     exit(1);
-  }
+	/* get the address of the host */
+	if((host_ptr = gethostbyname(serv_host)) == NULL) {
+		printf("gethostbyname error");
+		exit(1);
+	}
   
-  if(host_ptr->h_addrtype !=  AF_INET) {
-     perror("unknown address type");
-     exit(1);
-  }
-  
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = 
-     ((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
-  serv_addr.sin_port = htons(port);
-  
+	if(host_ptr->h_addrtype !=  AF_INET) {
+		printf("unknown address type");
+		exit(1);
+	}
 
-  /* open a TCP socket */
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-     perror("can't open stream socket");
-     exit(1);
-  }
+	/* address */
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = 
+		((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
+	serv_addr.sin_port = htons(port);
 
-  /* connect to the server */    
-  if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-     perror("can't connect to server");
-     exit(1);
-  }
+	/* open a TCP socket */
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("can't open stream socket");
+		exit(1);
+	}
+	printf("sockfd: %d\n", sockfd);	//dbg
 
-  /* write a message to the server */
-  write(sockfd, "hello world", sizeof("hello world"));
-    
-  close(sockfd);
+	/* connect to the server */    
+	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		printf("can't connect to server");
+		exit(1);
+	}
+//<asus-bob+>
+	for(;;) {
+		scanf("%d", &getdata);
+
+		if(0!=getdata)
+		{
+			/* write a message to the server */
+			sprintf(msg, "Hi Bob-%d!!", getdata);
+			printf("send [%s]\n", msg);	//dbg
+			write(sockfd, msg, sizeof(msg));
+
+			/* read a message from the client */
+			len = read(sockfd, msg, MAX_SIZE); 
+			/* make sure it's a proper string */
+			msg[len] = 0;
+			printf("%s\n", msg);
+
+			getdata=0;
+		}
+	}
+	close(sockfd);
+//  write(sockfd, "hello world", sizeof("hello world"));
+//<asus-bob->
 }
+
+
+//			strcpy(msg,"Hi Bob!!");
--- server.c
+++ server.c
@@ -5,60 +5,70 @@
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 
-#define SERV_TCP_PORT 8000 /* server's port number */
+#define SERV_TCP_PORT 8100 //<asus-bob+>8000	/* server's port number */
 #define MAX_SIZE 80
+#define	exit(X)	return(X)	//<asus-bob+>
 
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, clilen;
-  struct sockaddr_in cli_addr, serv_addr;
-  int port;
-  char string[MAX_SIZE];
-  int len;
-  
+	int		sockfd, newsockfd, clilen;
+	struct sockaddr_in cli_addr, serv_addr;
+	int		port;
+	char	string[MAX_SIZE];
+	int		len;
+	char	msg[200];	//<asus-bob+>
+
   /* command line: server [port_number] */
   
-  if(argc == 2) 
-     sscanf(argv[1], "%d", &port); /* read the port number if provided */
-  else
-     port = SERV_TCP_PORT;
+	if(argc == 2) 
+		sscanf(argv[1], "%d", &port); /* read the port number if provided */
+	else
+		port = SERV_TCP_PORT;
   
-  /* open a TCP socket (an Internet stream socket) */
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-     perror("can't open stream socket");
-     exit(1);
-  }
-  	
-  /* bind the local address, so that the cliend can send to server */
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(port);
+	/* open a TCP socket (an Internet stream socket) */
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("can't open stream socket");
+		exit(1);
+	}
+	printf("sockfd: %d\n", sockfd);	//dbg
+
+	/* bind the local address, so that the cliend can send to server */
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(port);
   
-  if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-     perror("can't bind local address");
-     exit(1);
-  }
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		printf("can't bind local address");
+		exit(1);
+	}
 	
-  /* listen to the socket */
-  listen(sockfd, 5);
+	/* listen to the socket */
+	listen(sockfd, 5);
 
-  for(;;) {
+	for(;;) {
+		/* wait for a connection from a client; this is an iterative server */
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-     /* wait for a connection from a client; this is an iterative server */
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		
-     if(newsockfd < 0) {
-        perror("can't bind local address");
-     }
+		printf("newsockfd: %d\n", newsockfd );	//dbg
+
+		if(newsockfd < 0) {
+			printf("can't bind local address");
+		}
    
-     /* read a message from the client */
-     len = read(newsockfd, string, MAX_SIZE); 
-     /* make sure it's a proper string */
-     string[len] = 0;
-     printf("%s\n", string);
-     
-     close(newsockfd);
-  }  
+		/* read a message from the client */
+		len = read(newsockfd, string, MAX_SIZE); 
+		/* make sure it's a proper string */
+		string[len] = 0;
+		printf("%s\n", string);
+//<asus-bob+>
+		/* write a message to the Client */
+		sprintf(msg, "    Server newsockfd:%d!!", newsockfd);
+		printf("    send [%s]\n", msg); //dbg
+		write(sockfd, msg, sizeof(msg));
+//		close(newsockfd);
+	}
+	close(newsockfd);
+//<asus-bob->
 }
