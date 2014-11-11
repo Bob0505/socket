3e38506d03c43965fe2a0e1503504b58e0568d8d
Bob3_Chen <Bob3_Chen@asus.com>
2014-11-11 17:39:47 +0800

    add sub function.

--- client.c
+++ client.c
@@ -11,57 +11,104 @@
 #define MAX_SIZE 80
 #define	exit(X)	return(X)	//<asus-bob+>
 
-int main(int argc, char *argv[])
+int TCPsocket(int *TCP_socketfd, struct sockaddr_in *serv_addr)
 {
-	int		sockfd;
-	struct sockaddr_in serv_addr;
-	char	*serv_host = "localhost";
 	struct hostent *host_ptr;
-	int		port;
-	int		buff_size = 0;
-	char	msg[MAX_SIZE];
-	int		getdata;
-	int		ret;
-	int		read_len;
+	char	*serv_host = "localhost";
 
-
-  /* command line: client [host [port]]*/
-	if(argc >= 2) 
-		serv_host = argv[1]; /* read the host if provided */
-	if(argc == 3)
-		sscanf(argv[2], "%d", &port); /* read the port if provided */
-	else 
-		port = SERV_TCP_PORT;
 
 	/* get the address of the host */
 	if((host_ptr = gethostbyname(serv_host)) == NULL) {
 		perror("gethostbyname error");
-		exit(1);
+		exit(-1);
 	}
-  
-	if(host_ptr->h_addrtype !=  AF_INET) {
+
+	if(host_ptr->h_addrtype !=	AF_INET) {
 		perror("unknown address type");
-		exit(1);
+		exit(-1);
 	}
 
 	/* address */
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = 
-		((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
-	serv_addr.sin_port = htons(port);
+	memset((char*)serv_addr, 0, sizeof(serv_addr));
+	serv_addr->sin_family		= AF_INET;
+	serv_addr->sin_addr.s_addr	= ((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
+	serv_addr->sin_port			= htons(SERV_TCP_PORT);
 
 	/* open a TCP socket */
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	*TCP_socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(*TCP_socketfd < 0) {
 		perror("can't open stream socket");
-		exit(1);
-	}
-	printf("open a TCP socket FD%d\n", sockfd);	//dbg
+		exit(-1);
+	}else
+		printf("open a TCP socket FD%d\n", *TCP_socketfd); //dbg
 
-	/* connect to the server */    
-	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+	exit(0);
+}
+
+int SendData(int TCP_socketfd, char* pSendData, int Send_len)
+{
+	int sent_len;
+
+	/* write a message to the server */
+	printf("send [%s]\n", pSendData); //dbg
+#if 1
+	sent_len = write(TCP_socketfd, pSendData, Send_len);
+#else
+	sent_len = send(TCP_socketfd, pSendData, Send_len, 0);
+#endif
+	if(sent_len != Send_len)
+		printf("Send_len: %d, but the sent_len: %d", Send_len, sent_len);
+
+	return sent_len;
+}
+
+int ReceiveData(int TCP_socketfd, char* pRevdata, int Recv_len)
+{
+	int	Recved_len;
+
+	memset(pRevdata, 0, Recv_len);
+	/* read a message from the client */
+#if 1
+	Recved_len = read(TCP_socketfd, pRevdata, Recv_len);
+#else
+	Recved_len = recv(TCP_socketfd, pRevdata, MAX_SIZE, 0);
+#endif
+	/* make sure it's a proper string */
+	switch(Recved_len)
+	{
+		case -1:
+//			perror("recv()");
+			break;
+		case 0:
+//			perror("Client disconnect");
+			break;
+		default:
+			printf("Receive: len=[%d] pRevdata=[%s]\n", Recved_len, pRevdata);
+			if(Recved_len != Recv_len)
+				printf("Recv_len: %d, but the Recved_len: %d", Recv_len, Recved_len);
+			break;
+		}
+
+	return Recved_len;
+}
+
+
+int main(int argc, char *argv[])
+{
+	int		TCP_socketfd;
+	struct sockaddr_in serv_addr;
+	int		getdata;
+	int		ret;
+	char	pSendData[MAX_SIZE], pRevdata[MAX_SIZE];
+	int		sent_len, Recved_len;
+
+	TCPsocket(&TCP_socketfd, &serv_addr);
+
+	/* connect to the server */
+	ret = connect(TCP_socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if(ret < 0) {
 		perror("can't connect to server");
-		exit(1);
+		exit(-1);
 	}
 
 	for(;;) {
@@ -74,22 +121,17 @@ int main(int argc, char *argv[])
 		if(0 != getdata)
 		{
 			/* write a message to the server */
-			sprintf(msg, "Hi Bob-%d!!", getdata);
-			printf("send [%s]\n", msg);	//dbg
-			ret = write(sockfd, msg, sizeof(msg));
-			if(sizeof(msg) != ret)
-				printf("write ret:%d\n", ret);	//dbg
+			sprintf(pSendData, "Hi Bob-%d!!", getdata);
+			sent_len = SendData(TCP_socketfd, pSendData, MAX_SIZE);
 
 			/* read a message from the client */
-			read_len = read(sockfd, msg, MAX_SIZE);
-			/* make sure it's a proper string */
-			msg[read_len] = 0;
-			printf("Receive: len=[%d] msg=[%s] for FD#%d\n", read_len, msg, sockfd);
+			Recved_len = ReceiveData(TCP_socketfd, pRevdata, MAX_SIZE);
 
 			getdata=0;
 		}
 	}
-	ret = close(sockfd);
+
+	ret = close(TCP_socketfd);
 	if(0 != ret)
 		printf("close ret:%d\n", ret);	//dbg
 }
--- server.c
+++ server.c
@@ -8,141 +8,182 @@
 
 #define SERV_TCP_PORT 8100 //<asus-bob+>8000	/* server's port number */
 #define MAX_SIZE 80
-#define	exit(X)	return(X)	//<asus-bob+>
+#define	exit(X)	return(X)
 
-int main(int argc, char *argv[])
+#define	TRUE	1
+#define	FALSE	0
+
+int TCPsocket_bind_listen(int *TCP_socketfd)
 {
-	int		sockfd, newsockfd, max_fd;
-	int		port, recv_len, sel_ret, index;
-	char	string[MAX_SIZE], msg[MAX_SIZE];
-	fd_set	active_fd_set, read_fds;
-	socklen_t	clilen = sizeof(struct sockaddr_in);
-	struct sockaddr_in	client_addr, serv_addr;
-	struct timeval		tv;
-	int		clean_FD;
 	int		ret;
-
-  /* command line: server [port_number] */
-  
-	if(argc == 2) 
-		sscanf(argv[1], "%d", &port); /* read the port number if provided */
-	else
-		port = SERV_TCP_PORT;
+	struct sockaddr_in	serv_addr;	
 
 	/* open a TCP socket (an Internet stream socket) */
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	*TCP_socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(*TCP_socketfd < 0) {
 		perror("can't open stream socket");
-		exit(1);
-	}
-	printf("Internet stream socket FD#%d\n", sockfd);	//dbg
+		exit(-1);
+	}else
+		printf("Internet stream socket FD#%d\n", *TCP_socketfd);
 
 	/* bind the local address, so that the cliend can send to server */
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(port);
+	memset((char*)&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family		= AF_INET;
+	serv_addr.sin_addr.s_addr	= htonl(INADDR_ANY);
+	serv_addr.sin_port			= htons(SERV_TCP_PORT);
 
-	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+	ret = bind(*TCP_socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if(ret < 0) {
 		perror("can't bind local address");
-		exit(1);
+		exit(-1);
 	}
 
 	/* listen to the socket */
-	if(listen(sockfd, 5) < 0) {
-        perror("listen()");
-		exit(1);
+	ret = listen(*TCP_socketfd, 5);
+	if(ret < 0) {
+		perror("listen()");
+		exit(-1);
 	}
 
-    FD_ZERO(&active_fd_set);
-    FD_SET(sockfd, &active_fd_set);
-    max_fd = sockfd;
+	exit(0);
+}
+
+int AcceptNewSockfd(int TCP_socketfd, int *newsockfd)
+{
+
+	struct sockaddr_in	client_addr;
+	socklen_t			clilen = sizeof(struct sockaddr_in);
+
+	*newsockfd = accept(TCP_socketfd, (struct sockaddr *) &client_addr, &clilen);
+	if (*newsockfd == -1) {
+		perror("accept()");
+		exit(-1);
+	} else
+		printf("Accpet client come from [%s:%u] by FD#%d\n",
+			inet_ntoa(client_addr.sin_addr),
+			ntohs(client_addr.sin_port),
+			*newsockfd);
+
+	exit(0);
+}
+
+int ReceiveData(int FD_index, int* clean_FD, char* pRevdata, int Recv_len)
+{
+	int	Recved_len;
+
+	memset(pRevdata, 0, Recv_len);
+	/* read a message from the client */
+#if 1
+	Recved_len = read(FD_index, pRevdata, Recv_len);
+#else
+	Recved_len = recv(FD_index, pRevdata, MAX_SIZE, 0);
+#endif
+	/* make sure it's a proper string */
+	switch(Recved_len)
+	{
+		case -1:
+//			perror("recv()");
+			*clean_FD = TRUE;
+			break;
+		case 0:
+//			perror("Client disconnect");
+			*clean_FD = TRUE;
+			break;
+		default:
+			printf("Receive: len=[%d] pRevdata=[%s] from FD#%d\n", Recved_len, pRevdata, FD_index);
+			*clean_FD = FALSE;
+			if(Recved_len != Recv_len)
+				printf("Recv_len: %d, but the Recved_len: %d", Recv_len, Recved_len);
+			break;
+		}
+
+	return Recved_len;
+}
+
+int SendData(int FD_index, char* pSendData, int Send_len)
+{
+	int sent_len;
+
+	/* write a message to the server */
+	printf("send [%s] by FD#%d\n", pSendData, FD_index); //dbg
+#if 1
+	sent_len = write(FD_index, pSendData, Send_len);
+#else
+	sent_len = send(FD_index, pSendData, Send_len, 0);
+#endif
+	if(sent_len != Send_len)
+		printf("Send_len: %d, but the sent_len: %d", Send_len, sent_len);
+
+	return sent_len;
+}
+
+int main(int argc, char *argv[])
+{
+	int		TCP_socketfd, max_fd, newsockfd;
+	int		ret, FD_index, clean_FD, Recved_len, sent_len;
+	fd_set	active_fd_set, read_fds;
+	struct timeval	tv;
+	char	pRevdata[MAX_SIZE], pSendData[MAX_SIZE];
+
+	ret = TCPsocket_bind_listen(&TCP_socketfd);
+	if(ret<0)	exit(ret);
+
+	//Initializes the file descriptor set fdset to have zero bits for all file descriptors.
+	FD_ZERO(&active_fd_set);
+	//Sets the bit for the file descriptor fd in the file descriptor set fdset.
+	FD_SET(TCP_socketfd, &active_fd_set);
+	max_fd = TCP_socketfd;
 
 	for(;;) {
 		/* Set timeout */
-		tv.tv_sec = 2;
-		tv.tv_usec = 0;
+		tv.tv_sec	= 2;
+		tv.tv_usec	= 0;
 
-        read_fds = active_fd_set;
-		sel_ret = select(max_fd + 1, &read_fds, NULL, NULL, &tv);
+		read_fds = active_fd_set;
 
-		if (sel_ret == -1) {
-			perror("select()");
-			exit(-1);
-		} else if (sel_ret == 0) {
-//			printf("select timeout\n");
-			continue;
-		} else {
-
-			/* Service all sockets */
-			for (index = 0; index < FD_SETSIZE; index++) {
-                if (FD_ISSET(index, &read_fds)) {
-					if (index == sockfd) {
-						/* Connection request on original socket. */
-						/* Accept */
-						newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clilen);
-						if (newsockfd == -1) {
-							perror("accept()");
-							exit(-1);
-						} else {
-							printf("Accpet client come from [%s:%u] by FD#%d\n",
-									inet_ntoa(client_addr.sin_addr),
-									ntohs(client_addr.sin_port),
-									newsockfd);
-
+		ret = select(max_fd + 1, &read_fds, NULL, NULL, &tv);
+		switch(ret)
+		{
+			case -1:
+//				perror("select()");
+				exit(-1);
+				break;
+			case 0:
+//				printf("select timeout\n");
+				continue;
+			default:
+				/* Service all sockets */
+				for (FD_index = 0; FD_index < max_fd+1; FD_index++) {	//FD_SETSIZE:1024
+					//Returns a non-zero value if the bit for the file descriptor fd is set in the file descriptor set by fdset, and 0 otherwise.
+					if (FD_ISSET(FD_index, &read_fds)) {
+						if (FD_index == TCP_socketfd) {
+							/* Connection request on original socket. */
+							AcceptNewSockfd(TCP_socketfd, &newsockfd);
 							/* Add to fd set */
 							FD_SET(newsockfd, &active_fd_set);
+							/* update max_fd */
 							if (newsockfd > max_fd)
 								max_fd = newsockfd;
-						}
-					} else {	// index!=sockfd
-						/* Data arriving on an already-connected socket */
 
-						/* Receive */
-						memset(string, 0, sizeof(string));
-						/* read a message from the client */
-#if 1
-						recv_len = read(index, string, sizeof(string)); 
-#else
-						recv_len = recv(index, string, sizeof(string), 0);
-#endif
-						/* make sure it's a proper string */
+						} else { // FD_index!=TCP_socketfd
+							/* Data arriving on an already-connected socket */
+							Recved_len = ReceiveData(FD_index, &clean_FD, pRevdata, MAX_SIZE);
 
-						if (recv_len == -1) {
-//							perror("recv()");
-							clean_FD = 1;
-						} else if (recv_len == 0) {
-//							perror("Client disconnect\n");
-							clean_FD = 1;
-						} else {
-							printf("Receive: len=[%d] msg=[%s] for FD#%d\n", recv_len, string, index);
-							/* Send (In fact we should determine when it can be written)*/
-
-							/* write a message to the server */
-							sprintf(string, "%s from Server", string);
-							printf("send [%s]\n", string); //dbg
-#if 1
-							ret = write(index, string, sizeof(string));
-#else
-							ret = send(index, string, recv_len, 0);
-#endif
-							if(sizeof(msg) != ret)
-								printf("write ret:%d\n", ret);	//dbg
-
-							clean_FD = 0;
-						}
-
-						if(clean_FD)
-						{
-							printf("FD#%d disconnect\n", index);
-							/* Clean up */
-							close(index);
-							FD_CLR(index, &active_fd_set);
-						}
-					} //if(i == sockfd)
-				} //if (FD_ISSET(index, &read_fds))
-			} //for (index = 0; index < FD_SETSIZE; index++)
-		} //if (sel_ret == -1) {
+							if(0 == clean_FD){
+								sprintf(pSendData, "%s from Server", pRevdata);
+								sent_len = SendData(FD_index, pSendData, MAX_SIZE);
+							}else{
+								printf("FD#%d disconnect\n", FD_index);
+								/* Clean up */
+								close(FD_index);
+								//Clears the bit for the file descriptor fd in the file descriptor set fdset.
+								FD_CLR(FD_index, &active_fd_set);
+							}
+						} //if(FD_index == TCP_socketfd)
+					} //if (FD_ISSET(FD_index, &read_fds))
+				} //for (FD_index = 0; FD_index < FD_SETSIZE; FD_index++)
+				break;
+		} //switch(ret)
 	} //for(;;)
-
 	exit(0);
 }
